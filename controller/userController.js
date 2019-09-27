d401eea8b44e34badabdb92cec153f62874ba85e
jinsoo49 <auhy49@gmail.com>
2019-09-28 03:06:57 +0900

    controller complete

--- controller/userController.js
+++ controller/userController.js
@@ -1,7 +1,34 @@
-export const join = (req, res) => res.render("join", { pageTitle: "Join" });
-export const login = (req, res) => res.render("login", { pageTitle: "Log In" });
-export const logout = (req, res) =>
-  res.render("logout", { pageTitle: "Log Out" });
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "Join" });
+};
+
+export const postJoin = (req, res) => {
+  const {
+    body: { name, email, password, password2 }
+  } = req;
+  if (password !== password2) {
+    res.status(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    // ToDo : Register User
+    // ToDo : Log user in
+    res.redirect(routes.home);
+  }
+};
+
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Log In" });
+
+export const postLogin = (req, res) => {
+  res.redirect(routes.home);
+};
+
+export const logout = (req, res) => {
+  // ToDo : Process Log Out
+  res.redirect(routes.home);
+};
 export const userDetail = (req, res) =>
   res.render("userDetail", { pageTitle: "User Detail" });
 export const editProfile = (req, res) =>
---
+++ db.js
@@ -0,0 +1,54 @@
+export const videos = [
+  {
+    id: 325123,
+    title: "Video awesome",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 111333,
+      name: "Jinsoo",
+      email: "jin@soo.com"
+    }
+  },
+  {
+    id: 151232,
+    title: "Video awesome2",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 111333,
+      name: "Jinsoo",
+      email: "jin@soo.com"
+    }
+  },
+  {
+    id: 848975,
+    title: "Video awesome3",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 111333,
+      name: "Jinsoo",
+      email: "jin@soo.com"
+    }
+  },
+  {
+    id: 888402,
+    title: "Video awesome4",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 111333,
+      name: "Jinsoo",
+      email: "jin@soo.com"
+    }
+  }
+];
